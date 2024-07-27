4fc71890130a1d5bb7de78dbe26e99155309ff2b
Артём <129595378+ArtemShevchenko2023@users.noreply.github.com>
2024-07-27 13:59:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,16 +1,162 @@
+# Импорты
 import telebot
+import speech_recognition as sr
+import os
+import ffmpeg
+import pyttsx3
+from pydub import AudioSegment
 from config import *
 from logic import *
+# Запуск и хэндлеры
 bot = telebot.TeleBot(bot)
+language = 'ru'
+# Создание клавиатуры для смены языка и других команд
+def create_keyboard():
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(telebot.types.KeyboardButton('/lang ru'), telebot.types.KeyboardButton('/lang en'))
+    keyboard.add(telebot.types.KeyboardButton('/help'), telebot.types.KeyboardButton('/start'))
+    return keyboard
+# Команда /start с клавиатурой других команд
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Для помощи введи /help")
+    if language == 'ru':
+        welcome_text = "Привет! Для помощи введи /help"
+    else:
+        welcome_text = "Hello! Type /help for assistance"
+    bot.send_message(message.chat.id, welcome_text, reply_markup=create_keyboard())
+# Команда /help с клавитурой для других команд
 @bot.message_handler(commands=['help'])
-def send_welcome(message):
-    bot.reply_to(message, "Вот список моих команд:\n/start - приветствие\n/help - все команды\nНапиши любое сообщение с запросом что хочешь увидеть в изображении!")
+def send_help(message):
+    if language == 'ru':
+        help_text = "Вот список моих команд:\n/start - приветствие\n/help - все команды\n/lang - смена языка\n/text - ответ только текстом\n/image - ответ только картинкой\n/prof - опишите что любите делать\n/voice - озвучка сообщения голосом бота\nОтправь любое голосовое сообщение и я отвечу на него!\nНапиши любое сообщение и я отвечу на него!"
+    else:
+        help_text = "Here is a list of my commands:\n/start - greeting\n/help - all commands\n/lang - change language\n/text - answer only text\n/image - answer only image\n/prof - describe what you can do\n/voice - convert text in bot voice\nSend me any voice message and I will respond to it!\nSend me any message and I will respond to it!"
+    bot.send_message(message.chat.id, help_text, reply_markup=create_keyboard())
+# Команда для смены языка
+@bot.message_handler(commands=['lang'])
+def set_language(message):
+    global language
+    if len(message.text.split()) > 1:
+        lang = message.text.split()[1].lower()
+        if lang in ['ru', 'en']:
+            language = lang
+            if language == 'ru':
+                bot.send_message(message.chat.id, "Язык установлен на Русский.")
+            else:
+                bot.send_message(message.chat.id, "Language set to English.")
+        else:
+            bot.send_message(message.chat.id, "Пожалуйста, выберите язык: /lang ru/en")
+    else:
+        bot.send_message(message.chat.id, "Пожалуйста, укажите язык: /lang ru/en\nPlease, enter language: /lang ru/en")
+# Генерирование текста
+@bot.message_handler(commands=['text'])
+def send_text(message):
+    text = message.text
+    gen = generate(f"{text}")
+    save_request_response(message.from_user.id, text, gen)
+    bot.send_message(message.chat.id, f"{gen}")
+# Генерирование картинок
+@bot.message_handler(commands=['image'])
+def send_img(message):
+    text = message.text
+    api = Text2ImageAPI(fus, ap, secret)
+    api.conv(text)
+    img = open('decoded.jpg', 'rb')
+    bot.send_photo(message.chat.id, img)
+# Выбор профессии, просто перечислите что нравится
+@bot.message_handler(commands=['prof'])
+def send_text(message):
+    text = message.text
+    gen = generate(f"Какая профессия мне подходит? Я занимаюсь {text}!")
+    save_request_response(message.from_user.id, text, gen)
+    bot.send_message(message.chat.id, f"{gen}")
+# Обработчик всех голосовых сообщений
+@bot.message_handler(content_types=['voice'])
+def handle_voice(message):
+    file_info = bot.get_file(message.voice.file_id)
+    downloaded_file = bot.download_file(file_info.file_path)
+    temp_voice_file = 'voice.ogg'
+    with open(temp_voice_file, 'wb') as f:
+        f.write(downloaded_file)
+    recognizer = sr.Recognizer()
+    result = ""
+    os.system(f'ffmpeg -i {temp_voice_file} voice.wav')
+    with sr.AudioFile('voice.wav') as source:
+        audio = recognizer.record(source)
+        if language == 'ru':
+            try:
+                result = recognizer.recognize_google(audio, language='ru-RU')
+                bot.send_message(message.chat.id, result)
+                text = result
+                gen = generate(f"{text}")
+                save_request_response(message.from_user.id, text, gen)
+                bot.send_message(message.chat.id, f"{gen}")
+            except sr.UnknownValueError:
+                bot.reply_to(message, "Не удалось распознать аудио.")
+            except sr.RequestError:
+                bot.reply_to(message, "Ошибка соединения с сервисом.")
+        else:
+            try:
+                result = recognizer.recognize_google(audio, language='en-US')
+                bot.send_message(message.chat.id, result)
+                text = result
+                gen = generate(f"{text}")
+                save_request_response(message.from_user.id, text, gen)
+                bot.send_message(message.chat.id, f"{gen}")
+            except sr.UnknownValueError:
+                bot.reply_to(message, "Unknown value")
+            except sr.RequestError:
+                bot.reply_to(message, "Request error")
+    if os.path.isfile(temp_voice_file):
+        os.remove(temp_voice_file)
+    if os.path.isfile('voice.wav'):
+        os.remove('voice.wav')
+# Озвучка текста голосом бота и ответ ИИ
+@bot.message_handler(commands=['voice'])
+def send_voice(message):
+    if language == 'ru':
+        try:
+            text = message.text.split(' ', 1)[1]
+            if len(text) <= 500:
+                engine = pyttsx3.init()
+                engine.save_to_file(text, 'voice_message.mp3')
+                engine.runAndWait()
+                with open('voice_message.mp3', 'rb') as voice:
+                    bot.send_audio(message.chat.id, voice)
+                gen = generate(f"{text}")
+                save_request_response(message.from_user.id, text, gen)
+                bot.send_message(message.chat.id, f"{gen}")
+                os.remove('voice_message.mp3')
+            else:
+                bot.send_message(message.chat.id, "Ваш текст слишком большой!")
+        except Exception as e:
+            bot.send_message(message.chat.id, "Что-то не так, попробуй снова!")
+    else:
+        try:
+            text = message.text.split(' ', 1)[1]
+            if len(text) <= 500:
+                engine = pyttsx3.init()
+                engine.save_to_file(text, 'voice_message.mp3')
+                engine.runAndWait()
+                with open('voice_message.mp3', 'rb') as voice:
+                    bot.send_audio(message.chat.id, voice)
+                gen = generate(f"{text}")
+                save_request_response(message.from_user.id, text, gen)
+                bot.send_message(message.chat.id, f"{gen}")
+                os.remove('voice_message.mp3')
+            else:
+                bot.send_message(message.chat.id, "Your text >= 500!")
+        except Exception as e:
+            bot.send_message(message.chat.id, "Something went wrong!")
+# Ответ на все текстовые сообщения
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
     text = message.text
-    gen = generate(text)
-    bot.reply_to(message, f"{gen}")
+    gen = generate(f"{text}")
+    save_request_response(message.from_user.id, text, gen)
+    bot.send_message(message.chat.id, f"{gen}")
+    api = Text2ImageAPI(fus, ap, secret)
+    api.conv(text)
+    img = open('decoded.jpg', 'rb')
+    bot.send_photo(message.chat.id, img)
 bot.infinity_polling()
